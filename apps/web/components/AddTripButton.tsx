0e201445970d74c6b326e444db8f50b39702cb9a
Brian Nelson <briannelson95@proton.me>
2025-06-11 11:43:42 -0400

    added text on button for large screens

--- apps/web/components/AddTripButton.tsx
+++ apps/web/components/AddTripButton.tsx
@@ -16,9 +16,10 @@ export default function AddTripButton() {
             <div className='fixed bottom-4 right-4 z-40'>
                 <button 
                     onClick={handleOpenModal} 
-                    className='bg-blue-600 rounded-full aspect-square flex justify-center items-center p-3 hover:bg-blue-500 transition-colors duration-200'
+                    className='bg-blue-600 rounded-full aspect-square flex justify-center xl:gap-2 items-center p-3 xl:px-4 xl:py-3 hover:bg-blue-500 transition-colors duration-200 xl:aspect-auto'
                 >
                     <PlusIcon />
+                    <p className='hidden xl:block text-lg font-semibold'>Request Trip</p>
                 </button>
             </div>
             <AddTripModal show={showModal} onClose={handleOpenModal} />
